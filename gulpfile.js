b235fc94b8edb265887f111dd48cbd80b2b32ab5
neverhood671 <nastya-art@yandex.ru>
2018-10-20 21:49:09 +0300

    init project

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('html', function() {
+    return gulp.src('src/view/main.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('build/html'))
+});
+
+
+gulp.task('sass', function() {
+    return gulp.src('src/view/components/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concatCss("bundle.css"))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('build/css'));
+});
+
+/*gulp.task('js', function(){
+  return gulp.src('client/javascript/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/js'))
+});*/
+
+
+gulp.task('default', [ 'html', 'sass' /*, 'js'*/ ]);
